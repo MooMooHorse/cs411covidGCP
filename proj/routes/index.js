188932cfff02548dbeac301254594d71e81f8269
MooMooHorse <haor.20@intl.zju.edu.cn>
2023-04-05 18:14:35 +0000

    enable insertion to userQuery table when user searches something

--- proj/routes/index.js
+++ proj/routes/index.js
@@ -104,13 +104,23 @@ router.post('/adquery1', function(req, res, next) {
         if (err) throw err;
         const isTokenValid = DB_CONFIG.isSignatureValid(token,username);
         if(isTokenValid) {
-            console.log('token is valid');
+            // console.log('token is valid');
             /**
              * @todo:
              * store the query and the result in the database, now we hae user name, query and query result.
              */
+            const insert_userQuery_table = `
+            INSERT INTO covidgcp.userQuery (username, queryContent, queryType, queryResult, resultIndex)
+            VALUES ('${username}', '${queriedState}', 'ADQ1', '${result[0]}', 1);
+            `;
+            // console.log(insert_userQuery_table);
+            con.query(insert_userQuery_table, function(err, result) {
+                if (err) throw err;
+                // console.log(result);
+            });
+
         }
-    // console.log(result[0]);
+        // console.log(result[0]);
         res.send(result[0]);
     });
 });
